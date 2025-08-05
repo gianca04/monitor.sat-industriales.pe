a20db82964386d289dc0abd501147280986473c3
gianca04 <grancarlosavalogallardo@gmail.com>
2025-08-05 11:32:30 -0500

    Observador para eliminar imagenes

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Models\Photo;
+use App\Observers\PhotoObserver;
+
+Photo::observe(PhotoObserver::class);
+
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\DashboardPanelProvider::class,
